4437e8318b52c4a0f0c44dc57e487bd92d802ed6
Cyril Bonté <cyril.bonte@free.fr>
2012-04-21 22:37:04 +0200

    Code cleanup

--- parser/seealso.py
+++ parser/seealso.py
@@ -2,13 +2,7 @@ import re
 import parser
 
 class Parser(parser.Parser):
-    def __init__(self, pctxt):
-        parser.Parser.__init__(self, pctxt)
-        #template = pctxt.templates.get_template("parser/seealso.tpl")
-        #self.replace = template.render().strip()
-
     def parse(self, line):
-        #return re.sub(r'(See also *:)', self.replace, line)
         pctxt = self.pctxt
 
         result = re.search(r'(See also *:)', line)
